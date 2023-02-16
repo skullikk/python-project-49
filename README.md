65e2c9d8aa82167822912089e4ffb9ba30e5e4f1
Andrey Cherepanov <skullikk@mail.ru>
2023-02-16 09:13:56 +0300

    step 5

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 [![Actions Status](https://github.com/skullikk/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/skullikk/python-project-49/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/b74a577bc9ecb959a3ca/maintainability)](https://codeclimate.com/github/skullikk/python-project-49/maintainability)
+
+
+https://asciinema.org/a/5BsZX1P5fR7qdAQtFUxDypFEF
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,26 @@
+import prompt
+from random import randint
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        number_ = randint(1, 100)
+        print(f'Question: {number_}')
+        result = number_ % 2 == 0 and 'yes' or 'no'
+        user_answer = prompt.string('Your answer: ')
+        if user_answer != result or user_answer not in ['yes', 'no']:
+            print(f'\'{user_answer}\' is wrong answer ;(. Correct answer was \'{result}\'.')
+            print(f'Let\'s try again, {name}!')
+            break
+        else:
+            print('Correct!')
+    else:
+        print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
