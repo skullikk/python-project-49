ba0a9cccb6b5a92b0747b8990805757d9c46fcf8
Andrey Cherepanov <skullikk@mail.ru>
2023-02-17 13:57:58 +0300

    step 8

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import prompt
+from random import randint
+from brain_games.cli import welcome_user, answer_incorrect
+
+
+def progression():
+    start = randint(1, 10)
+    lenght = randint(5, 10)
+    step = randint(2, 10)
+    index = randint(0, lenght-1)
+    stop = start + lenght * step
+    list_ = list(range(start, stop, step))
+    number_ = list_[index]
+    list_[index] = '..'
+    print('Question: ', end='')
+    print(*list_, sep=' ')
+    return number_    
+
+
+def main():
+    name = welcome_user()
+    print('What number is missing in the progression?')
+    for i in range(3):
+        result = progression()
+        user_answer = prompt.integer('Your answer: ')
+        if user_answer != result:
+            answer_incorrect(result, user_answer, name)
+            break
+        else:
+            print('Correct!')
+    else:
+        print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
